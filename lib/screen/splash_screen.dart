6596a1ce61aeb0c3d541e05531ff9b61308f739c
sidrathore <rathoresiddharth0207@gmail.com>
2023-01-31 10:38:13 +0530

    automatic navigation of splash screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -2,7 +2,22 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mybloodbank_app/screen/on_bording_page1.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() {
+    return Screen();
+  }
+}
+
+class Screen extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(
+      Duration(seconds: 3),
+      () => Navigator.pushNamed(context,'/onboarding1'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,11 +25,7 @@ class SplashScreen extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Container(
-            child: InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, '/onboarding1');
-                },
-                child: Image.asset('assets/images/Rectangle 7.png')),
+            child: Image.asset('assets/images/Rectangle 7.png'),
           ),
         ),
       ),
